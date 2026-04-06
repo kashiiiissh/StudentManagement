b4c284fb8e16df078407d43dedc330ad5f1463b6
kashiiiissh <kashishkashyap012004@gmail.com>
2026-04-06 14:32:47 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -5,8 +5,8 @@ import java.sql.SQLException;
 public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/studentdb";
-    private static final String USER = "kashish";
-    private static final String PASSWORD = "Kashish01!";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
 
     public static Connection getConnection() {
         Connection con = null;
